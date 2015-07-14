48eaaf5f0277e453dd371a75456dbf1385cd91ee
Tymoteusz Jankowski <tymoteusz.jankowski@ext.allegro.pl>
2015-07-14 10:35:47 +0200

    Polymer in Tipboard - PoC
    This needs a lot of changes, however it works (PoC).
    This also should be py3 ready.

--- tipboard/app.py
+++ tipboard/app.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 import os
 import json
 import logging
@@ -19,64 +11,10 @@ import tornado.websocket
 import tornadoredis
 
 from tipboard import settings
-from tipboard.api import api_version, MetaProperty, TileData, ProjectInfo, Push
-from tipboard.parser import process_layout_config, get_config_files_names
+from tipboard.api import Push2
 from tipboard.redis_utils import db_events_path
 
 log = logging.getLogger(__name__)
-
-
-class Flipboard(object):
-
-    def __init__(self):
-        self.last_found_configs_number = -1
-        self.paths = []
-
-    def get_paths(self):
-        """
-        Returns url paths to dashboards (created from .yaml config from
-        userspace).
-        """
-        config_names = self._get_config_names()
-        if len(config_names) != self.last_found_configs_number:
-            self.paths = self._config_names2paths(config_names)
-            self.last_found_configs_number = len(config_names)
-        return self.paths
-
-    def get_flipboard_title(self):
-        """
-        Returns title to display as a html title.
-        """
-        title = ''
-        config_names = self._get_config_names()
-        if len(config_names) == 1:
-            config = process_layout_config(config_names[0])
-            try:
-                title = config['details']['page_title']
-            except KeyError:
-                msg = 'config {} has no key: details/page_title'.format(
-                    config_names[0]
-                )
-                log.error(msg)
-        elif len(config_names) > 1:
-            # TODO: put here more suitable title?
-            title = 'Flipboard Mode'
-        return title
-
-    def _get_config_names(self):
-        config_names = settings.FLIPBOARD_SEQUENCE
-        if not any(config_names):
-            config_names = get_config_files_names()
-            if not any(config_names):
-                raise Exception('No config (.yaml) file found in ~/.tipboard/')
-        return config_names
-
-    def _config_names2paths(self, config_names):
-        paths = []
-        for name in config_names:
-            path = '/' + name
-            paths.append(path)
-        return paths
 
 
 class RedisMixin(object):
@@ -95,49 +33,28 @@ class DashboardSocketHandler(tornado.websocket.WebSocketHandler, RedisMixin,
     """Handles client connections on web sockets and listens on a Redis
     subscription."""
 
-    cache = set()   # Only cache keys. Values will be retrieved from Redis
-                    # every time. This ensures users don't get stale data.
-    tipboard_helpers = {
-        'color': settings.COLORS,
-        'log_level': settings.JS_LOG_LEVEL,
-    }
+    def check_origin(self, origin):
+        # TODO:: rm it, it's tmp
+        return True
+
+    # Only cache keys. Values will be retrieved from Redis
+    # every time. This ensures users don't get stale data.
+    cache = set()
 
     def __init__(self, *args, **kwargs):
         super(DashboardSocketHandler, self).__init__(*args, **kwargs)
         self.listen()
         self.getter = self.setup_redis()
 
-    def allow_draft76(self):
-        # for iOS 5.0 Safari
-        return True
-
-    def on_close(self):
-        log.info('Web socket closed.')
-        if self.pubsub.subscribed:
-            self.pubsub.unsubscribe(db_events_path())
-            self.pubsub.disconnect()
-        self.getter.disconnect()
-
-
     @tornado.gen.engine
-    def on_message(self, message):
-        log.info('Message received: %s.', message)
-        if message != 'update':
-            return
-        stale_keys = set()
-        for tile_id in self.cache:
-            log.debug('Putting data for tile: {}'.format(tile_id))
-            raw = yield tornado.gen.Task(self.getter.get, tile_id)
-            if not raw:
-                log.warn('No data in key %s on Redis.', tile_id)
-                stale_keys.add(tile_id)
-                continue
-            data = json.loads(raw)
-            data['tipboard'] = self.tipboard_helpers
-            self.write_message(data)
-        if stale_keys:
-            self.cache.difference_update(stale_keys)
-
+    def listen(self):
+        log.info('Web socket opened.')
+        self.pubsub = self.setup_redis()
+        yield tornado.gen.Task(
+            self.pubsub.subscribe, db_events_path()
+        )
+        self.pubsub.listen(self.on_publish)
+        log.info('Subscribed to %s on Redis.' % db_events_path())
 
     @tornado.gen.engine
     def on_publish(self, msg):
@@ -155,102 +72,44 @@ class DashboardSocketHandler(tornado.websocket.WebSocketHandler, RedisMixin,
                 log.warn('No data in key %s on Redis.', tile_id)
                 return
             data = json.loads(raw)
-            data['tipboard'] = self.tipboard_helpers
             self.cache.add(tile_id)
             self.write_message(data)
             log.info('Sent new data for %s through the Web socket.', tile_id)
 
     @tornado.gen.engine
-    def listen(self):
-        log.info('Web socket opened.')
-        self.pubsub = self.setup_redis()
-        yield tornado.gen.Task(
-            self.pubsub.subscribe, db_events_path()
-        )
-        self.pubsub.listen(self.on_publish)
-        log.info('Subscribed to %s on Redis.' % db_events_path())
-
-
-class DashboardRendererHandler(tornado.web.RequestHandler, SentryMixin):
-    def get(self, layout_name):
-        def _verify_statics(static_file):
-            user_tiles_path = os.path.join(
-                os.path.expanduser('~'), '.tipboard/custom_tiles'
-            )
-            tipboard_tiles_path = os.path.join(settings.TIPBOARD_PATH, 'tiles')
-            found = False
-            for path in user_tiles_path, tipboard_tiles_path:
-                if os.path.exists(os.path.join(path, static_file)):
-                    found = True
-                    break
-            return found
-
-        def _tile_path(tile_name):
-            """
-            Searches for tile's html file (in user's 'custom_tiles' folder,
-            and then in app's 'tiles' folder) and returns full path of
-            the tile, or raises exception if html file is not present in none
-            of those locations.
-            """
-            user_tiles_path = os.path.join(
-                os.path.expanduser('~'), '.tipboard/custom_tiles'
-            )
-            tipboard_tiles_path = os.path.join(settings.TIPBOARD_PATH, 'tiles')
-            tile_html = '.'.join((tile_name, 'html'))
-            for path in user_tiles_path, tipboard_tiles_path:
-                tile_path = os.path.join(path, tile_html)
-                if os.path.exists(tile_path):
-                    return tile_path
-            raise UserWarning('No such tile: %s' % tile_name)
-
-        try:
-            config = process_layout_config(layout_name or 'layout_config')
-        except IOError as e:
-            msg = '<br>'.join([
-                '<div style="color: red">',
-                'No config file found for dashboard: {}'.format(layout_name),
-                'Make sure that file: "{}" exists.'.format(e.filename),
-                '</div>',
-            ])
-            self.write(msg)
+    def on_message(self, message):
+        log.info('Message received: %s.', message)
+        if message != 'update':
             return
+        stale_keys = set()
+        for tile_id in self.cache:
+            log.debug('Putting data for tile: {}'.format(tile_id))
+            raw = yield tornado.gen.Task(self.getter.get, tile_id)
+            if not raw:
+                log.warn('No data in key %s on Redis.', tile_id)
+                stale_keys.add(tile_id)
+                continue
+            data = json.loads(raw)
+            self.write_message(data)
+        if stale_keys:
+            self.cache.difference_update(stale_keys)
 
-        tiles_js = ['.'.join((name, 'js')) for name in config['tiles_names']]
-        tiles_js = filter(_verify_statics, tiles_js)
-        tiles_css = ['.'.join((name, 'css')) for name in config['tiles_names']]
-        tiles_css = filter(_verify_statics, tiles_css)
-        self.render(
-            'layout.html',
-            details=config['details'],
-            layout=config['layout'],
-            tipboard_css=settings.TIPBOARD_CSS_STYLES,
-            tipboard_js=settings.TIPBOARD_JAVASCRIPTS,
-            tiles_css=tiles_css,
-            tiles_js=tiles_js,
-            tile_path=_tile_path,
-        )
-
-    def head(self):
-        # muted
-        pass
-
-
-class GetDashboardsPaths(tornado.web.RequestHandler):
-    def post(self):
-        paths = flipboard.get_paths()
-        jsoned = json.dumps({'paths': paths})
-        self.set_header("Content-Type", 'application/json')
-        self.write(jsoned)
+    def on_close(self):
+        log.info('Web socket closed.')
+        if self.pubsub.subscribed:
+            self.pubsub.unsubscribe(db_events_path())
+            self.pubsub.disconnect()
+        self.getter.disconnect()
 
 
-class FlipboardHandler(tornado.web.RequestHandler, SentryMixin):
+class MainHandler(tornado.web.RequestHandler, SentryMixin):
     def get(self):
         self.render(
-            'flipboard.html',
-            page_title=flipboard.get_flipboard_title(),
-            tipboard_css=settings.TIPBOARD_CSS_STYLES,
-            tipboard_js=['js/lib/jquery.js', 'js/flipboard.js', 'js/lib/require.js'],
-            flipboard_interval=settings.FLIPBOARD_INTERVAL,
+            'dashboard.html',
+            # page_title=flipboard.get_flipboard_title(),
+            # tipboard_css=settings.TIPBOARD_CSS_STYLES,
+            # tipboard_js=['js/lib/jquery.js', 'js/flipboard.js'],
+            # flipboard_interval=settings.FLIPBOARD_INTERVAL,
         )
 
 
@@ -275,30 +134,21 @@ class MultiStaticFileHandler(tornado.web.StaticFileHandler, SentryMixin):
 
     @classmethod
     def get_version(cls, settings, path):
-    # temporarily muted
+        # temporarily muted
         return None
 
 
 urls = [
-    (r"/", FlipboardHandler),
-    (r"/flipboard/getDashboardsPaths", GetDashboardsPaths),
+    (r"/", MainHandler),
     (r"/communication/websocket", DashboardSocketHandler),
-    (r"/([a-zA-Z0-9_-]*)", DashboardRendererHandler),
-    (r"/api/{}/{}/tileconfig/([a-zA-Z0-9_-]+)".format(
-        api_version, settings.API_KEY), MetaProperty),
-    (r"/api/{}/{}/tiledata/([a-zA-Z0-9_-]+)".format(
-        api_version, settings.API_KEY), TileData),
-    (r"/api/{}/{}/info".format(api_version, settings.API_KEY), ProjectInfo),
-    (r"/api/{}/{}/push".format(api_version, settings.API_KEY), Push),
+    (r"/api/{}/{}/push".format(2, settings.API_KEY), Push2),
 ]
 
-flipboard = Flipboard()
 app = tornado.web.Application(
     urls,
     template_path=os.path.join(settings.TIPBOARD_PATH, 'templates'),
     static_path=os.path.join(settings.TIPBOARD_PATH, "static"),
-    static_handler_class=MultiStaticFileHandler,
+    # static_handler_class=MultiStaticFileHandler,
     debug=settings.DEBUG,
-    tiles_paths=settings.TILES_PATHS,
     sentry_client=AsyncSentryClient(settings.SENTRY_DSN),
 )
--- tipboard/console.py
+++ tipboard/console.py
@@ -8,35 +8,17 @@ Usage:
     tipboard runserver
     tipboard runserver [<port>]
     tipboard runserver [<host>] [<port>]
-    tipboard create_project <name>
-    tipboard test --list | -l
-    tipboard test [<test_selector>]...
     tipboard (-h | --help)
     tipboard --version
 
-Arguments:
-    <test_selector> name of a test. Run "tipboard test --list" command to see
-    names.
-
 Options:
-    -l --list     Show list of tests.
     -h --help     Show this screen.
     --version     Show version.
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 import logging
 import logging.handlers
 import os
-import errno
-import shutil
-import sys
-import unittest
-import uuid
 
 from docopt import docopt
 import json
@@ -72,71 +54,14 @@ _setup_logger()
 log = logging.getLogger(__name__)
 
 
-def _create_project_dir(project_name):
-    user_config_dir = os.path.join(os.path.expanduser("~"), '.tipboard')
-    try:
-        os.mkdir(user_config_dir)
-    except OSError as e:
-        if all((e.errno == errno.EEXIST, os.path.isdir(user_config_dir))):
-            pass
-    custom_tiles_dir = os.path.join(user_config_dir, 'custom_tiles')
-    if not os.path.exists(custom_tiles_dir):
-        os.mkdir(custom_tiles_dir)
-
-
-def _create_local_settings(project_name):
-    user_config_dir = os.path.join(os.path.expanduser("~"), '.tipboard')
-    user_layout_config = os.path.join(user_config_dir, 'layout_config.yaml')
-    settings_local = os.path.join(user_config_dir, 'settings-local.py')
-    if not os.path.exists(user_layout_config):
-        shutil.copy2(
-            os.path.join(
-                settings.TIPBOARD_PATH, 'defaults/layout_config.yaml'  # noqa
-            ),
-            user_layout_config,
-        )
-    if not os.path.exists(settings_local):
-        shutil.copy2(
-            os.path.join(
-                settings.TIPBOARD_PATH, 'defaults/settings-local.py'
-            ),
-            settings_local
-        )
-        api_key = str(uuid.uuid4()).replace('-', '')
-        to_settings = (
-            "PROJECT_NAME = '%s'" % project_name,
-            "API_KEY = '%s'" % api_key,
-        )
-        with open(settings_local, 'a') as file_:
-            for item in to_settings:
-                file_.write('%s\n' % item)
-            file_.write('\n')
-    else:
-        error_msg = "Can't override file: %s" % settings_local
-        log.error(error_msg)
-        print(error_msg)
-
-
-def create_project(project_name):
-    user_config_dir = os.path.join(os.path.expanduser("~"), '.tipboard')
-    if not os.access(os.path.expanduser("~"), os.W_OK):
-        print('Your home directory is not writable. Aborting.')
-        return
-    if not os.path.isdir(user_config_dir):
-        _create_project_dir(project_name)
-        _create_local_settings(project_name)
-        print('Configuration files created in: %s' % user_config_dir)
-    else:
-        print('Configuration directory ~/.tipboard already exist. Aborting.')
-
-
 def runserver(address=None, port=None):
     def populate_key_cache():
         client = redis.StrictRedis(**settings.REDIS)
         keys = ':'.join([settings.PROJECT_NAME, 'tile', '*'])
         for key in client.keys(keys):
+            key = key.decode('utf8')
             try:
-                json.loads(client.get(key))
+                json.loads(client.get(key).decode('utf8'))
             except Exception:
                 log.warn('Key %s in Redis holds invalid data.', key)
                 continue
@@ -161,68 +86,6 @@ def runserver(address=None, port=None):
     tornado.ioloop.IOLoop.instance().start()
 
 
-def run_tests(test_names_list):
-    loader = unittest.TestLoader()
-    suite = unittest.TestSuite()
-    if test_names_list:
-        for test_name in test_names_list:
-            full_name = '.'.join(['tipboard.tests', test_name])
-            try:
-                found = loader.loadTestsFromName(full_name)
-                suite.addTests(found)
-            except (AttributeError, ImportError) as e:
-                print("No such test: {}".format(test_name))
-                print("Running tests - STOPPED.")
-                print("Run:\n  {}\nto see list of tests.".format(
-                    "tipboard test --list"
-                ))
-                return False
-    else:
-        tests_path = os.path.join(settings.TIPBOARD_PATH, 'tests')
-        suite = unittest.TestLoader().discover(tests_path, pattern='test*.py')
-        if suite.countTestCases() == 0:
-            print("Could not find any tests.")
-            return False
-    # mute useless communicates
-    out_hdlr = open(os.devnull, "w")
-    sys.stdout = out_hdlr
-    logging.disable(logging.CRITICAL)
-    unittest.TextTestRunner(verbosity=2).run(suite)
-
-
-def show_test_names():
-    """
-    Prints available tests list, easy to be copied and run.
-    """
-    def _found_tests():
-        tests_path = os.path.join(settings.TIPBOARD_PATH, 'tests')
-        suite = loader.discover(tests_path, pattern='test*.py')
-        for file_tests in suite._tests:
-            for class_tests in file_tests._tests:
-                for test in class_tests._tests:
-                    yield test
-
-    print('Available test names:')
-    loader = unittest.TestLoader()
-    last_test_file_name, last_test_case_name = '', ''
-    for test in _found_tests():
-        test_file_name, test_case_name = (
-            str(test).split(' ')[1][1:-1].split('.')
-        )
-        test_name = test._testMethodName
-        if last_test_file_name != test_file_name:
-            to_print = '  ' + test_file_name
-            print(to_print)
-            last_test_file_name = test_file_name
-        if last_test_case_name != test_case_name:
-            to_print = '  {}'.format(
-                '.'.join([to_print, test_case_name])
-            )
-            print(to_print)
-            last_test_case_name = test_case_name
-        print('  {}'.format('.'.join([to_print, test_name])))
-
-
 def main():
     arguments = docopt(
         __doc__,
@@ -232,14 +95,6 @@ def main():
         runserver(
             address=arguments.get('<host>'), port=arguments.get('<port>')
         )
-    if arguments.get('create_project'):
-        create_project(arguments.get('<name>'))
-
-    if arguments.get('test'):
-        if arguments.get('--list'):
-            show_test_names()
-        else:
-            run_tests(arguments.get('<test_selector>'))
 
 if __name__ == '__main__':
     main()
